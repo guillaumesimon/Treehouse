d1308f508063efd1d98fc4203a4e0365d0db470b
Guillaume <guillaume@tigerlilyapps.com>
2013-10-29 22:39:53 +0100

    Created a rails application and addedstatus update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treehouse::Application.config.secret_key_base = '87ec0575bd1ef2e76665bd12a66c7559fa77619d287bb6edc4b3d243fc3134fc7ddced2c9e4a14c97c008444c1f897d18a25d4b557edf36938827444832b2242'
